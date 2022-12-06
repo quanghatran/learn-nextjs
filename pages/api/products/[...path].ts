11ed949c1dee7335a45fa9e1153b35cd7336d745
quanghatran <quanghatran2161998@gmail.com>
2022-12-06 22:23:45 +0700

    proxy requests to API server

--- pages/api/products/[...path].ts
+++ pages/api/products/[...path].ts
@@ -1,10 +1,29 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next';
+import httpProxy from 'http-proxy';
 
-type Data = {
-  name: string;
+// type Data = {
+//   name: string;
+// };
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  res.status(200).json({ name: 'Catch all paths of products' });
+const proxy = httpProxy.createProxyServer();
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  req.headers.cookie = 'req.header';
+
+  console.log('check running server');
+
+  proxy.web(req, res, {
+    target: process.env.API_URL,
+    changeOrigin: true,
+    selfHandleResponse: false,
+  });
+
+  // res.status(200).json({ name: 'Catch all paths of products' });
 }
