3d7bbf09c42ebaeee2efffead4a48f2a44d5a19f
quanghatran <quanghatran2161998@gmail.com>
2023-05-17 02:05:23 +0700

    Implement footer

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,9 +1,32 @@
-import { Box } from '@mui/material';
+import { Facebook, Instagram, LinkedIn, Twitter } from '@mui/icons-material';
+import { Box, Icon, Stack, Typography } from '@mui/material';
 
 export function Footer() {
+  const socialLinks = [
+    { icon: Facebook, url: 'https://www.google.com/' },
+    { icon: Instagram, url: 'https://www.google.com/' },
+    { icon: Twitter, url: 'https://www.google.com/' },
+    { icon: LinkedIn, url: 'https://www.google.com/' },
+  ];
+
   return (
     <Box component="footer" py={2} textAlign="center">
-      Footer
+      <Stack direction="row" justifyContent="center">
+        {socialLinks.map((item, index) => (
+          <Box
+            key={index}
+            component="a"
+            p={2}
+            href={item.url}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Icon component={item.icon} sx={{ fontSize: 48 }} />
+          </Box>
+        ))}
+      </Stack>
+
+      <Typography>Copyright ©{new Date().getFullYear()} All rights reserved</Typography>
     </Box>
   );
 }
