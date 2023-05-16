2c56ab5261e8e63b761a9106111f879861f4ca9f
quanghatran <quanghatran2161998@gmail.com>
2023-05-17 02:23:48 +0700

    Config SEO support

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { Seo } from '@/components/common';
 import { Box } from '@mui/material';
 import { FeatureWorks, HeroSection, RecentPosts } from 'components/home';
 import { MainLayout } from 'components/layout';
@@ -18,6 +19,14 @@ const Home: NextPageWithLayout = () => {
 
   return (
     <Box>
+      <Seo
+        data={{
+          title: 'NextJS tutorials | Quang Haa',
+          description: 'Learn NextJS throughout the practice of a project from scratch.',
+          url: 'https://learn-nextjs-xi-two.vercel.app/',
+          thumbnailUrl: 'https://kanbox.vn/wp-content/uploads/2023/02/nextjs-cover.jpg',
+        }}
+      />
       <HeroSection />
       <RecentPosts />
       <FeatureWorks />
