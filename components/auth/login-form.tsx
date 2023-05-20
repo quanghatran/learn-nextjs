9071bf2c1e0ef096aee9460aba1098b027af092a
quanghatran <quanghatran2161998@gmail.com>
2023-05-20 23:33:25 +0700

    Config Input field

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,8 +1,11 @@
-import { Box } from '@mui/material';
+import { Box, Button, IconButton, InputAdornment } from '@mui/material';
 import { useForm } from 'react-hook-form';
 import { InputField } from '../form';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { MouseEvent, useState } from 'react';
 
 export function LoginForm() {
+  const [showPassword, setShowPassword] = useState(false);
   const { control, handleSubmit } = useForm({
     defaultValues: {
       username: '',
@@ -17,7 +20,27 @@ export function LoginForm() {
   return (
     <Box component="form" onSubmit={handleSubmit(handleLoginSubmit)}>
       <InputField name="username" control={control} label="label" />
-      <InputField name="password" control={control} label="label" />
+      <InputField
+        type={showPassword ? 'text' : 'password'}
+        name="password"
+        control={control}
+        label="label"
+        InputProps={{
+          endAdornment: (
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={() => setShowPassword((x) => !x)}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          ),
+        }}
+      />
+
+      <Button type="submit">Submit</Button>
     </Box>
   );
 }
--- components/form/input-fields.tsx
+++ components/form/input-fields.tsx
@@ -1,16 +1,37 @@
-import { TextField } from '@mui/material';
+import { TextField, TextFieldProps } from '@mui/material';
 import { Control, useController } from 'react-hook-form';
 
-export interface InputFieldProps {
+export type InputFieldProps = TextFieldProps & {
   name: string;
-  label: string;
   control: Control<any>;
-}
-export function InputField({ name, label, control }: InputFieldProps) {
+};
+
+export function InputField({
+  name,
+  label,
+  control,
+  value: externalValue,
+  onChange: externalOnChange,
+  onBlur: externalOnBlur,
+  ref: externalRef,
+  ...rest
+}: InputFieldProps) {
   const {
     field: { onChange, onBlur, value, ref },
     fieldState: { error },
   } = useController({ name, control });
 
-  return <TextField />;
+  return (
+    <TextField
+      fullWidth
+      size="small"
+      name={name}
+      margin="normal"
+      {...rest}
+      value={value}
+      onChange={onChange}
+      onBlur={onBlur}
+      inputRef={ref}
+    />
+  );
 }
