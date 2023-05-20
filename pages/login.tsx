41aa8f99b66a09035a9f52b65c4610d0869c96aa
quanghatran <quanghatran2161998@gmail.com>
2023-05-21 00:28:02 +0700

    Redirect to homepage after logged in

--- pages/login.tsx
+++ pages/login.tsx
@@ -11,44 +11,12 @@ export default function LoginPage() {
     revalidateOnMount: false,
   });
 
-  async function handleLoginClick() {
-    try {
-      await login({
-        username: 'QuangHaaa',
-        password: 'passworld@1234',
-      });
-
-      // redirect to dashboard
-      router.push('/about');
-    } catch (error) {
-      console.log('failed to login');
-    }
-  }
-
-  async function handleLogoutClick() {
-    try {
-      await logout();
-
-      // redirect to login page
-    } catch (error) {
-      console.log('failed to get profile');
-    }
-  }
-
-  // async function handleLogoutClick() {
-  //   try {
-  //     await authApi.logout();
-  //   } catch (error) {
-  //     console.log('failed to logout');
-  //   }
-  // }
-
   async function handleLoginSubmit(payload: LoginPayload) {
     try {
       await login(payload);
 
       // redirect to dashboard
-      router.push('/about');
+      router.push('/');
     } catch (error) {
       console.log('failed to login');
     }
